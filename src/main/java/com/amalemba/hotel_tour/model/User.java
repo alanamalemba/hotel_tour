2c063bce6d14050a15afe3c415fbd1fa4ff9157c
Alan Amalemba <alan.amalemba@lctafrica.net>
2025-05-16 10:58:29 +0300

    add user model

---
+++ src/main/java/com/amalemba/hotel_tour/model/User.java
@@ -0,0 +1,31 @@
+package com.amalemba.hotel_tour.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity // Marks this class as a JPA entity that maps to a database table
+@Table(name = "users") // Explicitly sets the table name to "users" (snake_case and pluralized for consistency and )
+@Data // Lombok: generates getters, setters, equals(), hashCode(), toString(), etc.
+@NoArgsConstructor // Lombok: generates a no-args constructor (required by JPA)
+@AllArgsConstructor // Lombok: generates a constructor with all fields
+
+public class User {
+
+    @Id // Marks this field as the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Uses MySQL's AUTO_INCREMENT feature
+    private Long id;
+
+    @Column(nullable = false) // Cannot be null in the database
+    private String name;
+
+    @Column(nullable = false, unique = true) // Must be unique and not null
+    private String email;
+
+    @Column(unique = true) // Must be unique, but can be null
+    private String phoneNumber;
+
+    @Column(nullable = false) // Password is required
+    private String password;// todo[]: implement hashing, find out how to use bcrypt for hashing
+}
