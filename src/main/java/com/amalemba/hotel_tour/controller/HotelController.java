99648418cc2d40fbc059330220ea464c08306631
Alan Amalemba <alan.amalemba@lctafrica.net>
2025-05-21 16:47:17 +0300

    remove unused imports

--- src/main/java/com/amalemba/hotel_tour/controller/HotelController.java
+++ src/main/java/com/amalemba/hotel_tour/controller/HotelController.java
@@ -1,14 +1,11 @@
 package com.amalemba.hotel_tour.controller;
 
 import com.amalemba.hotel_tour.model.Hotel;
-import com.amalemba.hotel_tour.security.UserPrincipal;
 import com.amalemba.hotel_tour.service.HotelService;
 import com.amalemba.hotel_tour.util.ResponseBuilder;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
